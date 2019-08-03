e08314228fc4b29116fb995175737ad3e3ec4371
tbrito1986v2 <agethiagoage@gmail.com>
2019-08-03 06:41:49 -0300

    Cliente end point

---
+++ src/main/java/com/thiagobrito/xbrain/services/ClienteService.java
@@ -0,0 +1,21 @@
+package com.thiagobrito.xbrain.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.thiagobrito.xbrain.domain.Cliente;
+import com.thiagobrito.xbrain.repositories.ClienteRepository;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+
+	public Cliente find(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
