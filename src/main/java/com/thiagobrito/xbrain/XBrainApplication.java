534bd7e55a451dc89beb85de3d40012d5a2bded3
thiago1986v2 <agethiagoage@gmail.com>
2019-08-03 03:09:02 -0300

    Projeto criado

---
+++ src/main/java/com/thiagobrito/xbrain/XBrainApplication.java
@@ -0,0 +1,13 @@
+package com.thiagobrito.xbrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XBrainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XBrainApplication.class, args);
+	}
+
+}
